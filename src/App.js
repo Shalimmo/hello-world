30a826462ba199996410021814193ea9c108b0b8
Shalim Mora <shalim@Mac-Lawrence.local>
2021-10-24 00:11:48 -0500

    Shalim Mora + lint

--- src/App.js
+++ src/App.js
@@ -8,19 +8,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Shalim Mora es la MAXIMA.
+          Shalim Mora es lo MAXIMO.
           <hr />
         </p>
         <br />
         <p>Lawrence is the best :)</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
